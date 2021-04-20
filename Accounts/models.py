cef28e0af799906031b76d0da6a81430e51a6d5f
Harsh Agarwal <harsh29092000@gmail.com>
2021-04-20 09:51:05 +0530

    Updated models

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,51 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
 class Customer(models.Model):
-	name=models.CharField(max_length=200,null=True)
-	phone_number=models.CharField(max_length=12,null=True)
-	email=models.EmailField(blank=True)
-	address=models.CharField(max_length=500,null=True)
-	date=models.DateTimeField(auto_now_add=True,null=True)
+	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+	name = models.CharField(max_length=200, null=True)
+	phone = models.CharField(max_length=200, null=True)
+	email = models.CharField(max_length=200, null=True)
+	profile_pic = models.ImageField(default="profile1.png", null=True, blank=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
 	def __str__(self):
 		return self.name
 
+
 class Tag(models.Model):
-	name=models.CharField(max_length=200,null=True)
+	name = models.CharField(max_length=200, null=True)
 
 	def __str__(self):
 		return self.name
 
 class Product(models.Model):
-	CATEGORY=(
-			('Fruits','Fruits'),
-			('Vegetables','Vegetables'),
-		)
+	CATEGORY = (
+			('Indoor', 'Indoor'),
+			('Out Door', 'Out Door'),
+			) 
 
-	name=models.CharField(max_length=200,null=True)
-	price=models.FloatField(null=True)
-	category=models.CharField(max_length=200,null=True,choices=CATEGORY)
-	description=models.CharField(max_length=500,null=True,blank=True)
-	date_added=models.DateTimeField(auto_now_add=True,null=True)
-	Tag=models.ManyToManyField(Tag)
+	name = models.CharField(max_length=200, null=True)
+	price = models.FloatField(null=True)
+	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+	description = models.CharField(max_length=200, null=True, blank=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	tags = models.ManyToManyField(Tag)
 
 	def __str__(self):
 		return self.name
 
-
-
-
 class Order(models.Model):
-	STATUS=(
-			('Pending','Pending'),
-			('Out for Delivery','Out for Delivery'),
-			('Delivered','Delivered'),
+	STATUS = (
+			('Pending', 'Pending'),
+			('Out for delivery', 'Out for delivery'),
+			('Delivered', 'Delivered'),
 			)
 
-	customer=models.ForeignKey(Customer,null=True,on_delete=models.SET_NULL)
-	product=models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
-	date_created=models.DateTimeField(auto_now_add=True,null=True)
-	status=models.CharField(max_length=200,null=True,choices=STATUS)
+	customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	status = models.CharField(max_length=200, null=True, choices=STATUS)
+	note = models.CharField(max_length=1000, null=True)
 
-	
+	def __str__(self):
+		return self.product.name
