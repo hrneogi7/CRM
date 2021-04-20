bbf34c3a2c61ba14bb4e24305605ca5d34458058
Harsh Agarwal <harsh29092000@gmail.com>
2021-04-20 14:37:38 +0530

    bug fixes

--- CRM/settings.py
+++ CRM/settings.py
@@ -132,5 +132,6 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
